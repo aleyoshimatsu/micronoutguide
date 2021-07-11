2917c7abeaba02b6078210ebd80896f25f1a39ba
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2021-07-11 13:11:36 -0300

    Primeiro commit

---
+++ src/main/kotlin/dev/alexandreyoshimatsu/example/micronaut/Application.kt
@@ -0,0 +1,10 @@
+package dev.alexandreyoshimatsu.example.micronaut
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+    build()
+        .args(*args)
+        .packages("example.micronaut")
+        .start()
+}
